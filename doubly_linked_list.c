8c61233dfa8209a5bd1f2a9b1240aac6718273dc
Michele Rossi <m.rossi@overside.it>
2022-11-27 08:47:49 +0100

    add doubly linked list (wip)

--- doubly_linked_list.c
+++ doubly_linked_list.c
@@ -1,13 +1,12 @@
 #include <stddef.h> // required for NULL
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 #define PPCAST(x) (struct list_node **)(x)
 #define PCAST(x) (struct list_node *)(x)
 
 struct list_node
 {
+    struct list_node *prev;
     struct list_node *next;
 };
 
@@ -21,33 +20,6 @@ struct list_node *list_get_tail(struct list_node **head)
         current_node = current_node->next;
     }
     return last_node;
-}
-
-struct list_node *list_append(struct list_node **head, struct list_node *item)
-{
-    struct list_node *tail = list_get_tail(head);
-    if (!tail)
-    {
-        *head = item;
-    }
-    else
-    {
-        tail->next = item;
-    }
-    item->next = NULL;
-    return item;
-}
-
-struct list_node *list_pop(struct list_node **head)
-{
-    struct list_node *current_head = *head;
-    if (!current_head)
-    {
-        return NULL;
-    }
-    *head = (*head)->next;
-    current_head->next = NULL;
-    return current_head;
 }
 
 struct list_node *list_get_node(struct list_node **head, const unsigned int index)
@@ -68,6 +40,22 @@ struct list_node *list_get_node(struct list_node **head, const unsigned int inde
     }
 
     return NULL;
+}
+
+struct list_node *list_append(struct list_node **head, struct list_node *item)
+{
+    struct list_node *tail = list_get_tail(head);
+    if (!tail)
+    {
+        *head = item;
+    }
+    else
+    {
+        tail->next = item;
+    }
+    item->prev = tail;
+    item->next = NULL;
+    return item;
 }
 
 struct list_node *list_remove(struct list_node **head, struct list_node *item)
@@ -99,22 +87,25 @@ struct list_node *list_remove(struct list_node **head, struct list_node *item)
     return NULL;
 }
 
-struct list_node *list_reverse(struct list_node **head)
+void list_insert_after(struct list_node **head, struct list_node *item, struct list_node *after)
 {
-    struct list_node *current = *head;
-    struct list_node *next = NULL;
-    struct list_node *prev = NULL;
-    while (current != NULL)
+    if (item == NULL)
     {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+        return;
     }
 
-    *head = prev;
-    return *head;
+    struct list_node *curr_next = item->next;
+    item->next = after;
+    after->next = curr_next;
 }
+
+// struct list_node *list_insert_before(struct list_node **head, struct list_node *item, struct list_node *before)
+// {
+// }
+
+// struct list_node *list_shuffle(struct list_node **head)
+// {
+// }
 
 struct string_item
 {
@@ -142,11 +133,7 @@ int main()
     list_append(PPCAST(&my_linked_list), PCAST(string_item_new("Test002")));
     list_append(PPCAST(&my_linked_list), PCAST(string_item_new("Last Item of the Linked List")));
 
-    list_reverse(PPCAST(&my_linked_list));
-
-    list_remove(PPCAST(&my_linked_list), list_get_node(PPCAST(&my_linked_list), 2));
-    list_remove(PPCAST(&my_linked_list), list_get_node(PPCAST(&my_linked_list), 1));
-    list_remove(PPCAST(&my_linked_list), list_get_node(PPCAST(&my_linked_list), 0));
+    list_insert_after(PPCAST(&my_linked_list), list_get_node(PPCAST(&my_linked_list), 10), PCAST(string_item_new("Test003")));
 
     struct string_item *string_item = my_linked_list;
     while (string_item)
