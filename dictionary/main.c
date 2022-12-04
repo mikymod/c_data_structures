9978f1c9c75f97e91f28a8e3e425b6875cab8082
Michele Rossi <m.rossi@overside.it>
2022-12-04 02:26:06 +0100

    cleanup

--- dictionary/main.c
+++ dictionary/main.c
@@ -7,18 +7,18 @@ int main()
 {
     struct dictionary *dict = dictionary_new(2);
 
-    dictionary_insert(dict, "tool", 4, "fucking data");
-    dictionary_insert(dict, "the_smiths", 10, "fucking data");
-    dictionary_insert(dict, "radiohead", 9, "fucking data");
-    dictionary_insert(dict, "the_cure", 8, "fucking data");
-    dictionary_insert(dict, "rem", 3, "fucking data");
-    dictionary_insert(dict, "slipknot", 8, "fucking data");
-    dictionary_insert(dict, "pinkfloyd", 9, "fucking data");
-    dictionary_insert(dict, "idles", 5, "fucking data");
-    dictionary_insert(dict, "shame", 5, "fucking data");
-    dictionary_insert(dict, "fountain_dc", 11, "fucking data");
-    dictionary_insert(dict, "porcupine_tree", 14, "fucking data");
-    dictionary_insert(dict, "nick_cave", 9, "fucking data");
+    dictionary_insert(dict, "tool", 4, "some data");
+    dictionary_insert(dict, "the_smiths", 10, "some data");
+    dictionary_insert(dict, "radiohead", 9, "some data");
+    dictionary_insert(dict, "low", 3, "some data");
+    dictionary_insert(dict, "antony", 6, "some data");
+    dictionary_insert(dict, "flaming_lips", 12, "some data");
+    dictionary_insert(dict, "the_notwist", 11, "some data");
+    dictionary_insert(dict, "idles", 5, "some data");
+    dictionary_insert(dict, "shame", 5, "some data");
+    dictionary_insert(dict, "fontaines_dc", 12, "some data");
+    dictionary_insert(dict, "porcupine_tree", 14, "some data");
+    dictionary_insert(dict, "nick_cave", 9, "some data");
 
     int key = 12;
     dictionary_insert(dict, &key, sizeof(int), "fucking data");
